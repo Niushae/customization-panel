27593ba090d50d084b2360bf4e8f369e7c1953f2
Niushae <fabiana.moreno.fm@gmail.com>
2025-10-05 23:26:52 -0400

    refactor: remove unnecessary logic

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,19 +1,5 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useThemeStore } from '@/stores/theme'
-
-const themeStore = useThemeStore()
-const layoutStyles = computed(() => themeStore.cssVariables)
-</script>
-
 <template>
-  <footer
-    class="bg-gray-800 p-4 mt-8"
-    :style="{
-      color: 'var(--color-text-global)',
-      ...layoutStyles,
-    }"
-  >
+  <footer class="bg-gray-800 p-4 mt-8 text-gray-300">
     <div class="container mx-auto text-center text-gray-300">
       &copy; 2025 Customization Panel. All rights reserved.
     </div>
--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,19 +1,5 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { useThemeStore } from '@/stores/theme'
-
-const themeStore = useThemeStore()
-const layoutStyles = computed(() => themeStore.cssVariables)
-</script>
-
 <template>
-  <nav
-    class="bg-gray-800 p-4"
-    :style="{
-      color: 'var(--color-text-global)',
-      ...layoutStyles,
-    }"
-  >
+  <nav class="bg-gray-800 p-4 text-white">
     <div class="container mx-auto flex justify-between items-center">
       <div class="text-white text-xl font-bold">Theme Customizer</div>
       <div class="space-x-4">
